e4b451b42b0a1730bf6e708c11e3baa8bee97c60
FelipeJf05 <jorgefelipe.jf01@gmail.com>
2024-11-02 18:18:15 -0300

    ajuste main

--- src/main/java/com/proj/facul/ProjFaculApplication.java
+++ src/main/java/com/proj/facul/ProjFaculApplication.java
@@ -1,13 +1,15 @@
 package com.proj.facul;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class ProjFaculApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjFaculApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjFaculApplication.class, args);
+    }
 
 }
