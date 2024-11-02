c9de9a4e5c4676519455148a4a3974f4d557724b
FelipeJf05 <jorgefelipe.jf01@gmail.com>
2024-11-02 15:34:29 -0300

    Impl repositories

---
+++ src/main/java/com/proj/facul/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.proj.facul.repository;
+
+import com.proj.facul.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
