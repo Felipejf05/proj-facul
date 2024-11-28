683ac8b63dff5087ddf7041dc283305a43efd541
FelipeJf05 <jorgefelipe.jf01@gmail.com>
2024-11-28 17:31:21 -0300

    Ajuste na autenticação do usuário e melhoria nos logs

--- src/main/java/com/proj/facul/repository/UserRepository.java
+++ src/main/java/com/proj/facul/repository/UserRepository.java
@@ -4,22 +4,22 @@ import com.proj.facul.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByDocument(String document);
+
+    Optional<User> findByEmail(String email);
+
     boolean existsByDocument(String document);
-
     boolean existsByPhone(Long phone);
-
     boolean existsByEmail(String email);
-
     boolean existsByAddress(String address);
 
-    // Verificação de duplicidade para atualizações, ignorando o ID do usuário
     boolean existsByDocumentAndIdNot(String document, Long id);
-
     boolean existsByPhoneAndIdNot(Long phone, Long id);
-
     boolean existsByEmailAndIdNot(String email, Long id);
-
     boolean existsByAddressAndIdNot(String address, Long id);
 }
